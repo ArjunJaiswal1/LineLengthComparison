d8bc5fccfb4969e77ee3ccc215076ae74d9cf990
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-01-27 16:45:56 +0530

    [Arjun] Add ; Added Elapsed

--- LineLengthComparison/LineLengthComputation.cs
+++ LineLengthComparison/LineLengthComputation.cs
@@ -13,5 +13,18 @@ namespace LineLengthComparison
             double length = Math.Pow(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2), 0.5);
             return length;
         }
+        public void Compare(int a1, int b1, int c1, int a2, int b2, int c2)
+        {
+
+            // Function to check if they are equal
+            double length1 = this.CalculateLength(a1, b1, a2, b2);
+            double length2 = this.CalculateLength(c1, a1, c2, a2);
+
+            if (length1 == length2)
+                Console.WriteLine("The given straight" + " lines are equal");
+            else
+                Console.WriteLine("The given straight" + " lines are not equal");
+        }
     }
-} 
+}
+
--- LineLengthComparison/Program.cs
+++ LineLengthComparison/Program.cs
@@ -6,5 +6,12 @@ class program
         LineLengthComputation lineLength = new LineLengthComputation();
         double length = lineLength.CalculateLength(5, 8, 9, 10);
         Console.WriteLine(length);
+        int a1 = 4, b1 = 4,
+          c1 = 3, a2 = -6,
+          b2 = 2, c2 = 9;
+        lineLength.Compare(a1, b1, c1, a2, b2, c2);
+
+
     }
 }
+    
