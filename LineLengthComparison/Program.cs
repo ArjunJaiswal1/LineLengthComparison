3fa9297b37d8005548ceae9f9e8dcf67ea2d54bd
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-01-27 16:39:08 +0530

    [Arjun] Add ; Added Switched

---
+++ LineLengthComparison/Program.cs
@@ -0,0 +1,10 @@
+﻿using LineLengthComparison;
+class program
+{
+    static void Main(string[] args)
+    {
+        LineLengthComputation lineLength = new LineLengthComputation();
+        double length = lineLength.CalculateLength(5, 8, 9, 10);
+        Console.WriteLine(length);
+    }
+}
